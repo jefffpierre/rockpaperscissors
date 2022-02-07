b5846a8909a05329e32fe5633a063d41b2d8b830
Jeff Pierre <49200624+jefffpierre@users.noreply.github.com>
2022-02-07 01:55:07 +0000

    Added text to Rock Paper Scissors

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ scissors = '''
 #Write your code below this line 👇
 choice = int(input("Please enter 0 for Rock, 1 for Paper or 2 for Scissors ")) #Stored the user's input.
 choice2 = random.randint(0,2)
+print("\nLets Roll!")
 
 if choice == 0:
   print(rock)
@@ -63,6 +64,8 @@ elif choice == 2:
     print("Paper, dangit, you win!")
   elif choice2 == 2:
     print(scissors)
-    print("Both scissors, try again!")
+    print("Both scissors, try again!") 
 else:
   print("Did you choose 0, 1 or 2? Please try again. :)")
+
+print("\nThis was great, let's do it Again!")
